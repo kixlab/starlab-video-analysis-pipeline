dc25cb18620ca33bd27e52b2bb942b868547f040
fesiib <tlekbay.b@gmail.com>
2024-11-15 15:15:33 +0900

    cleanup comments

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -61,10 +61,6 @@ def get_response(messages, response_format="json_object", retries=1):
             break
         messages.append({"role": "assistant", "content": response.choices[0].message.content})
 
-    # print(f"Finish Reason: {finish_reason}")
-    # print(f"Usages: {usages}")
-    # print(f"Generated Text: {generated_text}")
-
     if response_format == "json_object":
         try:
             obj = json.loads(generated_text)
