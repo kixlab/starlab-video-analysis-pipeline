5599e833b5e5c630b84ab76e7c0779283ad8f261
fesiib <tlekbay.b@gmail.com>
2024-11-15 15:24:08 +0900

    try different openai models: default

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -13,7 +13,8 @@ client = OpenAI(
 
 SEED = 13774
 TEMPERATURE = 0
-MODEL_NAME = 'gpt-4o-2024-05-13'
+MODEL_NAME = 'gpt-4o-2024-08-06'
+# gpt-4o-2024-08-06
 # gpt-4o
 # gpt-4o-2024-05-13
 # chatgpt-4o-latest
